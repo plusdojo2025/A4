072dda5566507c76b09a98bb0b762b9d8d2e5380
miyotaplusdojo <miyota-kouhei-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 14:18:23 +0900

    Update Attendance.java

--- source/src/main/java/dto/Attendance.java
+++ source/src/main/java/dto/Attendance.java
@@ -1,5 +1,46 @@
 package dto;
 
-public class Attendance {
+import java.io.Serializable;
 
+public class Attendance implements Serializable{
+	private String name; // 氏名
+	private String number; //学籍番号
+	private String status; // 出席状況
+	private String attendanceDate; //出欠日
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getAttendanceDate() {
+		return attendanceDate;
+	}
+	public void setAttendanceDate(String attendanceDate) {
+		this.attendanceDate = attendanceDate;
+	}
+	public Attendance(String name, String number, String status, String attendanceDate) {
+		super();
+		this.name = name;
+		this.number = number;
+		this.status = status;
+		this.attendanceDate = attendanceDate;
+	}
+	public Attendance() {
+		super();
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+	
 }
