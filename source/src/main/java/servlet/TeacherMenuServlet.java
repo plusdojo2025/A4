d0b6ddd746bbcf80c059758fd23a1a1d86fb946b
plusdojo2025-ojima <ojima-kaori-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-16 16:11:50 +0900

    先生専用メニューservlet作成

--- source/src/main/java/servlet/TeacherMenuServlet.java
+++ source/src/main/java/servlet/TeacherMenuServlet.java
@@ -1,5 +1,34 @@
 package servlet;
 
-public class MenuServlet {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class TeacherMenuServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("teacherPw") == null) {
+			response.sendRedirect("/A4/TeacherLoginServlet");
+			return;
+		}
+		
+		
+		// メニューページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/A4/jsp/teacher_home.jsp");
+		dispatcher.forward(request, response);
+	}
 
 }
