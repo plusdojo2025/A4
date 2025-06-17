55feb39001c71a4496c35ac5ff7ea304cdc81d43
miyotaplusdojo <miyota-kouhei-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 17:07:09 +0900

    連絡DAO

--- source/src/main/java/dao/AnnouncementsDAO.java
+++ source/src/main/java/dao/AnnouncementsDAO.java
@@ -5,9 +5,112 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import dto.Announcemnts;
 
 public class AnnouncementsDAO {
+	
+	//連絡情報を閲覧する
+	public List<Announcemnts> select(Announcemnts card) {
+		Connection conn = null;
+		List<Announcemnts> cardList = new ArrayList<Announcemnts>();
 
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/a4?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"root", "password");
+
+			// SQL文を準備する
+			String sql = "SELECT * FROM Announcements WHERE className = ? ORDER BY announceDate DESC";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+			
+			pStmt.setString(1, card.getClassName());
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {
+				Announcemnts ann = new Announcemnts();
+                ann.setAnnounceId(rs.getString("announceId"));
+                ann.setClassName(rs.getString("className"));
+                ann.setAnnounce(rs.getString("announce"));
+                ann.setAnnounceDate(rs.getString("announceDate"));
+
+				cardList.add(ann);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			cardList = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			cardList = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					cardList = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return cardList;
+	}
+	
+	//連絡情報を登録する
+	public boolean insert(Announcemnts card) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/a4?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"root", "password");
+
+			// SQL文を準備する
+			String sql = "INSERT INTO Announcements (className, announce, announceDate) VALUES (?, ?, ?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			// SQL文を完成させる
+			
+			pStmt.setString(1, card.getClassName());
+			pStmt.setString(2, card.getAnnounce());
+			pStmt.setString(3, card.getAnnounceDate());
+
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return result;
+	}
 }
