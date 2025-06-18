a8c7dff02bfdf9fd07b1139867ef21beb58a1b01
miyotaplusdojo <miyota-kouhei-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 11:03:41 +0900

    teacher]S

--- source/src/main/java/servlet/TeacherAttendanceServlet.java
+++ source/src/main/java/servlet/TeacherAttendanceServlet.java
@@ -14,9 +14,38 @@ import javax.servlet.http.HttpSession;
 
 import dao.AttendanceDAO;
 import dto.Attendance;
-import dto.Tidpw;
+import dto.Sidpw;
 
 @WebServlet("/A4/TeacherAttendanceServlet")
-public class TeacherAttendanceServlet {
+public class TeacherAttendanceServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		//もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if(session.getAttribute("teacherName") == null && session.getAttribute("teacherPw") == null){
+			response.sendRedirect("A4/TeacherLoginServlet");
+			return;
+		}
+		//学籍番号取得
+		Sidpw studentInfo = new Sidpw();
+		studentInfo = (Sidpw)session.getAttribute("Sidpw");
+		String studentId = studentInfo.getNumber();
+		//その日の日付を取得
+		LocalDate today = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		String formattedDate = today.format(formatter);
+		//出席情報を取得
+		AttendanceDAO attendanceInfo = new AttendanceDAO();
+		Attendance attendance = new Attendance();
+		attendance = attendanceInfo.attendanceSelect(studentId, formattedDate);
+		
+		//リクエスト領域に保存
+		request.setAttribute("attendanceStatus", attendance);
+		
+		// ログインページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/A4/Pjsp/other_login.jsp");
+		dispatcher.forward(request, response);
+	}
 }
