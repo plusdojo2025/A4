72cd71584415de67cd41dce3c651930ca282d54d
miyotaplusdojo <miyota-kouhei-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-20 11:38:49 +0900

    ホーム連絡表示（生徒・保護者）

--- source/src/main/Webapp/WEB-INF/Pjsp/parent_home.jsp
+++ source/src/main/Webapp/WEB-INF/Pjsp/parent_home.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>    
 <!DOCTYPE html>
 <html>
 <head>
@@ -25,7 +26,8 @@
 	<div>
 		<!-- 最新のお知らせ表示 -->
 		<h2>最新のお知らせ</h2>
-		
+		<p>登録日時：<c:out value="${announcement.announceDate}" /></p>
+		<p>連絡内容：<c:out value="${announcement.announce}" /></p>
 	</div>
 	<footer>
 	</footer>
--- source/src/main/java/servlet/OtherMenuServlet.java
+++ source/src/main/java/servlet/OtherMenuServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import dao.AnnouncementsDAO;
+import dto.Announcemnts;
 
 public class OtherMenuServlet extends HttpServlet{
     private static final long serialVersionUID = 1L;
@@ -23,6 +27,16 @@ public class OtherMenuServlet extends HttpServlet{
 			return;
 		}
 
+		//生徒・保護者のホーム画面に最新の連絡登録情報を表示する
+		AnnouncementsDAO announceDao = new AnnouncementsDAO();
+		ArrayList<Announcemnts> announcelatest = new ArrayList<Announcemnts>();
+		Announcemnts annDto = new Announcemnts();
+		String announce = annDto.getAnnounce();
+		announcelatest = (ArrayList<Announcemnts>) announceDao.select(announce);
+		
+		//リクエストスコープへの保存
+		request.setAttribute("announcement", announcelatest);
+		
         // ホームページにフォワードする
         if(session.getAttribute("position").equals("生徒")) {
             RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/Sjsp/student_home.jsp");
