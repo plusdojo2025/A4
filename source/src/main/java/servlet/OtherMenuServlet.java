11671672a5b37b7e4e1b7714dca12541d70c9c19
FujitaManato <fujita-manato-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-24 14:39:40 +0900

    ｊｊｈ

--- source/src/main/java/servlet/OtherMenuServlet.java
+++ source/src/main/java/servlet/OtherMenuServlet.java
@@ -1,7 +1,6 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,9 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import dao.AnnouncementsDAO;
-import dto.Announcemnts;
 
 @WebServlet("/OtherMenuServlet")
 public class OtherMenuServlet extends HttpServlet{
@@ -25,22 +21,12 @@ public class OtherMenuServlet extends HttpServlet{
 		HttpSession session = request.getSession();
 		if (session.getAttribute("position") == null && session.getAttribute("studentName") == null ||
 			session.getAttribute("position") == null && session.getAttribute("parentName") == null) {
-			response.sendRedirect(request.getContextPath() +"/A4/OtherLoginServlet");
+			response.sendRedirect(request.getContextPath() +"/OtherLoginServlet");
 			return;
 		}
-System.out.println(session.getAttribute("position"));
-		//生徒・保護者のホーム画面に最新の連絡登録情報を表示する
-		AnnouncementsDAO announceDao = new AnnouncementsDAO();
-		ArrayList<Announcemnts> announcelatest = new ArrayList<Announcemnts>();
-		Announcemnts annDto = new Announcemnts();
-		String announce = annDto.getAnnounce();
-		announcelatest = (ArrayList<Announcemnts>) announceDao.select(announce);
-		
-		//リクエストスコープへの保存
-		request.setAttribute("announcement", announcelatest);
+
 		
         // ホームページにフォワードする
-
         if(session.getAttribute("position").equals("生徒")) {
             RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/Sjsp/student_home.jsp");
 		    dispatcher.forward(request, response);
